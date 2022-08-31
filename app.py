2791a38436a156182cef5f06c20e2da10f3565d7
HyungJoonYoon <hysy9255@gmail.com>
2022-08-31 17:00:59 +0900

    포크해서 clone하고 다시 푸시

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import pandas as pd 
+from sklearn.linear_model import LinearRegression
 
 raw_df = pd.read_csv('data\cups.csv')
 df = raw_df.drop(['third','fourth'], axis=1)
@@ -13,3 +14,14 @@ print(df.columns)
 print(df[df["goals_scored"]>100])
 
 #여기서 조금만 수정해 보겠습니다. 
+
+x = df["games"]
+y = df["goals_scored"]
+
+x = np.array(x).reshape(-1,1)
+y = np.array(y).reshape(-1,1)
+
+reg = LinearRegression().fit(x,y)
+
+print("coefficient: " + str(reg.coef_))
+print(f"intercept: {reg.intercept_}") 

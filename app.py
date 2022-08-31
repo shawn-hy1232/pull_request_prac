ce72a78fa0828b87feaca222c6e6b3337a7ff7b9
Shawn <shawn.hy1232@gmail.com>
2022-08-31 14:15:04 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import pandas as pd 
+
+raw_df = pd.read_csv('data\cups.csv')
+df = raw_df.drop(['third','fourth'], axis=1)
+
+print(df)
+print(df.columns)
+
+#employee_1은 여기서부터 작업해주세요! 
+#네 알겠습니다! 여기서부터 작업하겠습니다!
+
+print(df[df["goals_scored"]>100])
